bf1099d46a99701cf22e07ec082ba5777a65848b
zascerta <giammarco.daless@gmail.com>
2020-06-24 17:54:34 +0200

    fixed davids score, some cool  prints

--- net_analysis.py
+++ net_analysis.py
@@ -14,8 +14,8 @@ params = {
 	'Periods' : 260,
 	'firstDataPeriod' : 200,
 	'InitialDensity' :  1.7,
-	'NumFemales' : 4,                         # 4, 6, 9, 12, 15, 18, 22, 24
-	'NumMales' : 4,                           # 4, 6, 9, 12, 15, 18, 22, 24
+	'NumFemales' : 12,                         # 4, 6, 9, 12, 15, 18, 21, 24
+	'NumMales' : 12,                           # 4, 6, 9, 12, 15, 18, 21, 24
 	'Rating.Dom.female.Intensity' : 0.8,       # eg: 0.1  desp: 0.8
 	'Rating.Dom.male.Intensity' : 1.0,         # eg: 0.2  desp: 1.0
 	'female.PersSpace' : 2.0,
--- utils.py
+++ utils.py
@@ -66,10 +66,10 @@ def unifyRunsOutput(f_name):
 # The David's score for an individual i is given by:
 #	DS = w + w_2 - l - l_2 
 def davidsScore(contest_mat):
+	# compute win/fights ratio
 	n_ind = len(contest_mat[0])
 	P_mat = np.zeros((n_ind,n_ind))    # win proportion matrix
-	w = []                             # w values list
-	l = []                             # l values list
+	w = []                             # win ratio list
 	for i in range(n_ind):
 		P_list = [] 
 		for j in range(n_ind):
@@ -79,25 +79,42 @@ def davidsScore(contest_mat):
 			else:
 				a_ij = contest_mat[i][j]                      # no. times i defeat j
 				n_ij = a_ij + contest_mat[j][i]               # no. interactions between i and j
-				P_ij = (a_ij/n_ij if n_ij != 0 else 0)        # proportion of wins by i with j
+				P_ij = (a_ij/n_ij if n_ij != 0 else 0)        # proportion of wins by i over j
 				P_mat[i][j] = P_ij
 				P_list.append(P_ij)
 
-		w_i = sum(P_list)
-		l_i = sum([1 - P_list[idx] for idx in range(n_ind) if idx != i])
+		w_i = sum(P_list)              # i win rate 
 		w.append(w_i)
+
+	# compute loss/fights ratio
+	l = []                             # loss ratio list
+	for j in range(n_ind):
+		l_list = []
+		for i in range(n_ind):
+			l_list.append(P_mat[i][j])
+		
+		l_i = sum(l_list) 
 		l.append(l_i)
 
-	w_2 = []     # w2 values list
-	l_2 = []     # l2 values list
+	# compute term w_2 and l_2 to calculate David's Score
 	DS = []      # David's scores
+	w_2 = []     # w_2 values list
+	l_2 = []     # l_2 values list
 	for i in range(n_ind):	
+		w_2_i = []     
+		l_2_i = []     	
 		for j in range(n_ind):	
-			if j != i:
-				w_2_i = P_mat[i][j]*w[j]
-				l_2_i = P_mat[i][j]*l[j] 
-				w_2.append(w_2_i)
-				l_2.append(l_2_i)
+			if i == j:
+				w_2_i.append(0)
+				l_2_i.append(0)
+			else:
+				w_2_ij = P_mat[i][j]*w[j]
+				l_2_ij = P_mat[j][i]*l[j] 
+				w_2_i.append(w_2_ij)
+				l_2_i.append(l_2_ij)
+
+		w_2.append(sum(w_2_i))
+		l_2.append(sum(l_2_i))
 
 		DS_i = w[i] + w_2[i] - l[i] - l_2[i]
 		DS.append(DS_i) 
@@ -119,10 +136,10 @@ def hierarchySteepness(d_score):
 	DS = d_score['DS']
 	n_ind = len(DS)
 	for i in range(n_ind):
-		aux = (n_ind*(n_ind-1))/2
-		NormDS_i = (DS[i] + aux)/n_ind
+		NormDS_i = (DS[i] + (n_ind*(n_ind-1))/2)/n_ind
 		NormDS.append(NormDS_i)
 
+	print(NormDS)
 	tmp = NormDS.copy()
 	NormDS.sort(reverse=True)
 	ind_ids = []
@@ -132,16 +149,21 @@ def hierarchySteepness(d_score):
 				ind_ids.append('{}'.format(i+1))
 
 	ticks = [i for i in range(0,n_ind)]
-	x = np.array(ticks).reshape((-1, 1))
+	x = np.array(ticks) #.reshape((-1, 1))
 	y = np.array(NormDS)
 
-	model = LinearRegression().fit(x,y)
-	steepness = model.coef_ 
-	print('\nhierarchy steepness: %.4f' % abs(steepness))
+	from scipy import stats
+	slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
 
-	plotHierarchy(x,y,ind_ids)
+	#model = LinearRegression().fit(x,y)
+	#steepness = model.coef_ 
+	print('\nhierarchy steepness: %.4f' % slope)
+	print('intercept: %.4f' % intercept)
+	print('r_value: %.4f' % r_value)
 
-	return abs(steepness)
+	plotHierarchy(x,y,ind_ids,intercept,slope)
+
+	return abs(slope)
 
 
 # Reading the number of individuas from the config file
@@ -157,10 +179,11 @@ def individualsNumber(cfg_file):
 
 
 # plot hierarchy ranking
-def plotHierarchy(x, y, ind_ids):
+def plotHierarchy(x, y, ind_ids, intercept, slope):
 	n_ind = len(ind_ids)
 	fig, ax = plt.subplots()
-	ax.plot(x, y)
+	ax.plot(x, y, 'o-')
+	ax.plot(x, intercept + slope*x, '-', label='fitted line')
 	ax.set_xticks(np.arange(0, n_ind, 1))
 	ax.set_xticklabels(ind_ids)
 	ax.set(xlabel='individuals id', ylabel='normalized DS',
